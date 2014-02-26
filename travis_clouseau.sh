e171497aa9645478b2aaad1247880ee9133d05aa
Diego Lapiduz <diego.lapiduz@cfpb.gov>
2014-02-26 17:13:43 -0500

    Add travis clouseau shell script

---
+++ travis_clouseau.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Get rev-list
+if [ -n "$TRAVIS_COMMIT_RANGE" ]; then
+    REVS=`git rev-list --abbrev-commit $TRAVIS_COMMIT_RANGE | tr '\n' ' '`
+else
+    REVS=$TRAVIS_COMMIT
+fi
+
+# Download clouseau
+if [ ! -d "clouseau_run" ]; then
+    wget -O clouseau_run.tar.gz http://github.com/virtix/clouseau/archive/master.tar.gz
+    tar xfz clouseau_run.tar.gz
+    mv clouseau-master clouseau_run
+fi
+
+# Install clouseau
+cd clouseau_run
+pip install -r requirements.txt --use-mirrors
+
+# Run clouseau
+./bin/clouseau_thin -u https://github.com/$TRAVIS_REPO_SLUG --skip --dest $(dirname ../$(pwd)) --revlist="$REVS"

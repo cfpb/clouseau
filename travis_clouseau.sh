b2f0a909a37dc562c765a09e89c46a0234e8ce42
Diego Lapiduz <diego.lapiduz@cfpb.gov>
2014-02-27 13:54:48 -0500

    Trying a new curl

--- travis_clouseau.sh
+++ travis_clouseau.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+function json_escape(){
+  echo -n "$1" | python -c 'import json,sys; print json.dumps(sys.stdin.read())'
+}
+
 source ~/virtualenv/python2.7/bin/activate
 
 # Get rev-list
@@ -31,10 +35,10 @@ echo "Commit: $TRAVIS_COMMIT"
 echo "Running Clouseau"
 ./bin/clouseau_thin -u https://github.com/$TRAVIS_REPO_SLUG --skip --dest $(dirname ../$(pwd)) --revlist="$REVS" > .clouseau_output.txt
 
-curl -X POST -H "Content-Type: application/json" --data-binary @.clouseau_output.txt https://api.github.com/repos/$TRAVIS_REPO_SLUG/commits/$TRAVIS_COMMIT/comments?access_token=$BOTCLOUSEAU
-
 output=$(<.clouseau_output.txt)
 if [[ $output == *https://help.github.com/articles/remove-sensitive-data* ]]
 then
+  json_output=json_escape($output)
+  curl -X POST -H "Content-Type: application/json" -d "'{\"body\":\"$json_output\"}'" https://api.github.com/repos/$TRAVIS_REPO_SLUG/commits/$TRAVIS_COMMIT/comments?access_token=$BOTCLOUSEAU
   exit 1
 fi
